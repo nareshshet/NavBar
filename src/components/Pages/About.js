4905c9b6ff07d6ebb64fc78a7fd387bd1a8082b9
NARESH <nareshshet143@gmail.com>
2021-11-28 23:13:58 +0530

    First Message

---
+++ src/components/Pages/About.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      <h4>About</h4>
+      <br />
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa architecto
+        commodi doloribus, aut illo incidunt placeat inventore. Neque pariatur
+        nulla ducimus repudiandae recusandae, inventore repellat consectetur
+        ipsam quam veniam eius excepturi sed ipsum quia reiciendis architecto
+        autem reprehenderit officiis doloremque dolores molestias quasi atque.
+        Aut non adipisci reiciendis sed. Eum rem hic molestiae obcaecati cumque.
+        Eos consequatur ut iste molestias corrupti dolore. Nulla quae porro ut,
+        fugiat culpa ex, soluta beatae, maxime labore a sequi laudantium!
+        Excepturi cumque rerum qui odit delectus, voluptas nesciunt a quisquam
+        dolore dolor aut commodi praesentium illo unde temporibus sunt possimus,
+        voluptatibus, beatae iste eligendi repudiandae laudantium. Voluptas
+        repudiandae nisi laborum ipsum expedita accusamus dignissimos reiciendis
+        quo? Quasi debitis eius perspiciatis voluptate, eveniet omnis natus
+        nihil eligendi totam doloribus autem placeat libero, beatae soluta
+        necessitatibus dolores at numquam tenetur! Aperiam culpa nulla molestiae
+        et consequuntur magni rerum illum cupiditate, laboriosam optio, nemo
+        assumenda mollitia voluptatum ut? Explicabo voluptatum, suscipit tenetur
+        eius incidunt optio molestias neque veniam unde mollitia, nobis dolorum
+        eum architecto laborum itaque, dolores illo tempore modi exercitationem
+        voluptas aliquid earum ipsam consectetur omnis! Perspiciatis vero, nihil
+        ut alias saepe repellendus sapiente quibusdam quidem eum corporis.
+        Voluptatem repudiandae earum omnis consequuntur reiciendis, ratione
+        eius.
+      </p>
+    </div>
+  );
+};
---
+++ src/components/Pages/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam magni
+        perspiciatis animi facere laboriosam. Necessitatibus quaerat vero
+        quibusdam cumque inventore corrupti ipsa soluta dolor eligendi similique
+        officia ipsum sit dolorem, voluptate expedita odit. At ut sed voluptatem
+        officiis vitae incidunt blanditiis animi quaerat est voluptatibus ab
+        possimus suscipit, omnis sequi rem placeat aspernatur natus! Repellendus
+        amet iste dolor nam error numquam temporibus eius at a pariatur beatae
+        dolore libero quae vitae alias laudantium, fugiat sunt! Ea consequuntur
+        tenetur amet voluptate, itaque ab in quam nam animi libero a inventore
+        facere voluptatibus asperiores perferendis autem obcaecati quaerat
+        sequi! Quam nostrum animi perspiciatis optio consequatur quia ad odit
+        amet nisi officia quae consectetur deserunt, repudiandae placeat
+        repellendus nihil ducimus, sed ab ipsum, ullam enim accusantium minima!
+        Totam optio culpa hic commodi porro facere, aliquam modi cupiditate
+        libero. Autem animi, magnam aperiam quo provident dolorem modi placeat
+        laborum eveniet necessitatibus vitae sunt saepe amet consectetur quam
+        aliquid distinctio fugit mollitia ut, ducimus neque debitis. Quasi
+        tempora quibusdam alias ex at error amet maxime. Praesentium cupiditate
+        provident illo voluptatum debitis veniam quia rerum velit quod dolorem
+        rem culpa placeat repudiandae voluptatibus reiciendis, perspiciatis
+        voluptate enim sint repellat, a modi ratione iste beatae. Cupiditate,
+        accusantium.
+      </p>
+    </div>
+  );
+};
